d016c3d52ef62fe2aaf90e35f5ba67bd10cf7859
andru.quiroga.18@cnu.edu <andru.quiroga.18@cnu.edu>
2022-05-30 02:12:08 -0400

    Changes by capat

--- src/web_utils/check_model_status.py
+++ src/web_utils/check_model_status.py
@@ -6,7 +6,7 @@ def get_all_status():
     root_dir = '/opt/saltybet/saved_models/'
 
     try:
-        stdout = sh.grep(sh.ps("ax"), '-oP "(?:.py ).*"')
+        stdout = sh.grep(sh.ps("ax"), '-oP', '"(?:.py ).*"')
         active_model_full = stdout.split('\n')
         print(active_model_full)
         active_model_names = [x.split(' ')[-1] for x in active_model_full]
