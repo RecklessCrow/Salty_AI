d8bd57091ae663be17321506324535c6362dc617
andru.quiroga.18@cnu.edu <andru.quiroga.18@cnu.edu>
2022-05-30 05:45:57 -0400

    Pushed

--- src/control_panel_main.py
+++ src/control_panel_main.py
@@ -11,10 +11,10 @@ def get_all_status():
     output = subprocess.check_output(('grep', 'model_driver_main.py'), stdin=ps.stdout)
     ps.wait()
 
+    print(output)
+
     stdout = output
-
     active_model_names = [stdout.split()[-1].decode('utf-8') for stdout in stdout.splitlines()]
-
     saved_models = [f.name for f in os.scandir(root_dir) if f.is_dir()]
 
     blocks = []
