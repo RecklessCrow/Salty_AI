774b83ee1ce5b8937479ea8f805fd4472c8bb2d0
andru.quiroga.18@cnu.edu <andru.quiroga.18@cnu.edu>
2023-04-22 15:02:58 -0400

    Added Match History Listener

--- app/website/static/script.js
+++ app/website/static/script.js
@@ -88,3 +88,106 @@ $(document).ready(function () {
         eventSource.dispatchEvent(new MessageEvent('update', {data: JSON.stringify(lastEventData)}));
     }
 });
+
+
+function build_match_history_element(match_json) {
+
+    const {
+        red,
+        blue,
+        winner,
+        payout,
+    } = JSON.parse(match_json);
+
+    // Create the main container div
+    const container = document.createElement('div');
+    container.className = 'match-history-element';
+
+// Create the match-up div
+    const matchUp = document.createElement('div');
+    matchUp.className = 'match-history-match-up';
+
+// Create the left and right span elements for the match-up div
+    const leftSpan = document.createElement('span');
+    leftSpan.className = 'left red-text';
+    leftSpan.textContent = 'Red';
+
+    const middleSpan = document.createElement('span');
+    middleSpan.className = 'white-text';
+    middleSpan.textContent = 'VS';
+
+    const rightSpan = document.createElement('span');
+    rightSpan.className = 'right blue-text';
+    rightSpan.textContent = 'Blue';
+
+// Append the left, middle, and right spans to the match-up div
+    matchUp.appendChild(leftSpan);
+    matchUp.appendChild(middleSpan);
+    matchUp.appendChild(rightSpan);
+
+// Create the winner div
+    const winnerDiv = document.createElement('div');
+    winnerDiv.className = 'match-history-winner';
+
+// Create the "Winner:" and winner span elements for the winner div
+    const winnerLabel = document.createElement('span');
+    winnerLabel.className = 'white-text';
+    winnerLabel.textContent = 'Winner:';
+
+    const winnerName = document.createElement('span');
+    if (winner === 'red') {
+        winnerName.className = 'red-text';
+        winnerName.textContent = red;
+    } else {
+        winnerName.className = 'blue-text';
+        winnerName.textContent = blue;
+    }
+
+// Append the winner label and winner name to the winner div
+    winnerDiv.appendChild(winnerLabel);
+    winnerDiv.appendChild(winnerName);
+
+// Create the payout div
+    const payoutDiv = document.createElement('div');
+    payoutDiv.className = 'match-history-payout';
+
+// Create the "Payout:" and payout amount span elements for the payout div
+    const payoutLabel = document.createElement('span');
+    payoutLabel.className = 'white-text';
+    payoutLabel.textContent = 'Payout:';
+
+    const payoutAmount = document.createElement('span');
+    if (payout.includes('+')) {
+        payoutAmount.className = 'green-text';
+    } else {
+        payoutAmount.className = 'red-text';
+    }
+
+    payoutAmount.textContent = payout;
+
+// Append the payout label and payout amount to the payout div
+    payoutDiv.appendChild(payoutLabel);
+    payoutDiv.appendChild(payoutAmount);
+
+// Append the match-up, winner, and payout divs to the main container div
+    container.appendChild(matchUp);
+    container.appendChild(winnerDiv);
+    container.appendChild(payoutDiv);
+
+    return container;
+}
+
+eventSource.addEventListener('history_update', ({match_json}) => {
+
+    const matchElement = build_match_history_element(match_json);
+
+    // Prepend the new element to the beginning of the div
+    const div = document.getElementsByClassName('match-history');
+    div.insertBefore(matchElement, div.firstChild);
+
+    // Check if there are more than 10 elements in the div
+    if (div.children.length > 10) {
+        // If there are, remove the last element in the div
+        div.removeChild(div.lastChild);
+    }
+});
